5e1dab26f948195d479db66c970385c25a0953d8
Jihoonahn <jihoonahn.dev@gmail.com>
2023-06-02 00:19:37 +0900

    Make Project

---
+++ Targets/TuistTemplateKit/Tests/TuistTemplateKitTests.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+final class TuistTemplateKitTests: XCTestCase {
+    func test_example() {
+        XCTAssertEqual("TuistTemplateKit", "TuistTemplateKit")
+    }
+}
---
+++ Targets/TuistTemplateUI/Tests/TuistTemplateUITests.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+final class TuistTemplateUITests: XCTestCase {
+    func test_example() {
+        XCTAssertEqual("TuistTemplateUI", "TuistTemplateUI")
+    }
+}
