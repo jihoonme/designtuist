a9a647a3397edf07d618a3a51c683a7f0a4e30fd
jihoonahn <jihoonahn.dev@gmail.com>
2023-12-13 02:28:22 +0900

    Update Command

--- Sources/dt/DesignTuist.swift
+++ Sources/dt/DesignTuist.swift
@@ -1,10 +1,15 @@
 import Foundation
+import TSCBasic
+import DesignTuistKit
+import DesignTuistSupport
 
 @main
 private enum DesignTuist {
     static func main() async throws {
         if CommandLine.arguments.contains("--verbose") {
-
+            try? ProcessEnv.setVar(Constants.EnvironmentVariables.verbose, value: "true")
         }
+
+        await DesignTuistCommand.main()
     }
 }
