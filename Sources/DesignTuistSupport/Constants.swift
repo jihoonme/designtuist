8729d4b56542722d367310eed5bfd74e21f08a9c
jihoonahn <jihoonahn.dev@gmail.com>
2023-12-13 02:52:25 +0900

    Update Log

--- Sources/DesignTuistSupport/Constants.swift
+++ Sources/DesignTuistSupport/Constants.swift
@@ -6,5 +6,8 @@ public enum Constants {
     
     public enum EnvironmentVariables {
         public static let verbose = "DESIGN_TUIST_CONFIG_VERBOSE"
+        public static let detailedLog = "DESIGN_TUIST_CONFIG_DETAILED_LOG"
+        public static let osLog = "DESIGN_TUIST_CONFIG_OS_LOG"
+        
     }
 }
