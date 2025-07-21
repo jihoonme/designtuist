9eface0003013e28b2dd6546b19ca1beb3c621d0
jihoonahn <jihoonahn.dev@gmail.com>
2025-07-22 02:28:05 +0900

    Successful action Clean Service

--- Commands/CommandKit/Services/CleanService.swift
+++ Commands/CommandKit/Services/CleanService.swift
@@ -35,10 +35,16 @@ public final class CleanService {
 
     private func cleanXcodeProject(path: Path) {
         let folder = try? Folder(path: path)
-        let targets = folder?.allFolders(recursive: true).filter {
-            $0.extension == "xcodeproj" || $0.extension == "xcworkspace"
+        let targets = folder?.allFolders(recursive: true).filter { folder in
+            let name = folder.name
+            let pathComponents = folder.store.path.rawValue.split(separator: "/")
+            let parentName = pathComponents.dropLast().last.map(String.init) ?? ""
+            let isTopLevel = !parentName.hasSuffix(".xcodeproj") && !parentName.hasSuffix(".xcworkspace")
+            let isXcodeProject = name.hasSuffix(".xcodeproj") || name.hasSuffix(".xcworkspace")
+            return isXcodeProject && isTopLevel
         }
-        if let _ = targets?.isEmpty {
+
+        if targets?.isEmpty ?? true {
             logger.info("ℹ️ No .xcodeproj or .xcworkspace directories found.")
             return
         }
